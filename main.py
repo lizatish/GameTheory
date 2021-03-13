661182148ce7f48e67ad95ebd371d0fd12e135fd
Elizaveta Tishkovets <tiishka2@gmail.com>
2021-03-13 18:07:14 +0300

    Добавление считки с экселя

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import datetime
-import os
-import sys  # sys нужен для передачи argv в QApplication
+import sys
+
+import pandas as pd
 from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import QTableWidgetItem, QHeaderView
-from PyQt5.uic.properties import QtGui, QtCore
+from PyQt5.QtWidgets import QTableWidgetItem
 
 import design
 
@@ -36,24 +35,37 @@ class ExampleApp(QtWidgets.QWidget, design.Ui_Form):
                 if max_col_vals[idx] <= data[row][idx]:
                     self.saddle_points.append((row, idx))
 
-
         print(self.saddle_points)
-
-
-
 
     def load_data(self):
         filename = QtWidgets.QFileDialog.getOpenFileName(self, "Выберите фвйл (txt, csv)",
-                                                         "/home/liza/PycharmProjects/gameTheory", "txt (*.txt)")
-        if filename[0]:
-            f = open(filename[0], 'r')
-            with f:
-                data = f.read()
-        data = data.split('\n')[:-1]
-        data = [[elem for elem in line.split()] for line in data]
+                                                         "/home/liza/PycharmProjects/gameTheory",
+                                                         "xlsx (*.xlsx);; txt (*.txt)")
+
+        filename = filename[0]
+        ext = filename.split('.')[-1]
+        if ext == 'txt':
+            data = self.read_data_from_txt(filename)
+        else:
+            data = self.read_data_from_xlsx(filename)
 
         self.set_table_data(data)
         self.data = data
+
+    def read_data_from_xlsx(self, filename):
+        if filename:
+            df = pd.read_excel(filename, header=None)
+            data = df.values.tolist()
+            return data
+
+    def read_data_from_txt(self, filename):
+        if filename:
+            f = open(filename, 'r')
+            with f:
+                data = f.read()
+        data = data.split('\n')[:-1]
+        data = [[int(elem) for elem in line.split()] for line in data]
+        return data
 
     def set_table_data(self, data):
         numrows = len(data)
@@ -65,7 +77,7 @@ class ExampleApp(QtWidgets.QWidget, design.Ui_Form):
         self.table.clear()
         for row in range(numrows):
             for column in range(numcols):
-                self.table.setItem(row, column, QTableWidgetItem((data[row][column])))
+                self.table.setItem(row, column, QTableWidgetItem((str(data[row][column]))))
 
 
 def main():
